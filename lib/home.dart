f5381b23133c12e8924bf780d784e3ffd57e5563
ilyes <iluessahbeni11@gmail.com>
2021-11-15 08:23:27 +0100

    change the color +modify typo and lables styles

--- lib/home.dart
+++ lib/home.dart
@@ -96,25 +96,30 @@ class HomePage extends StatelessWidget {
         return Card(
           clipBehavior: Clip.antiAlias,
           // TODO: Adjust card heights (103)
-          child: Column(
-            // TODO: Center items on the card (103)
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 18 / 11,
-                child: Image.asset(
-                  product.assetName,
-                  package: product.assetPackage,
-                  // TODO: Adjust the box size (102)
-                  // TODO: Adjust the box size (102)
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
+// TODO: Change innermost Column (103)
+          children: <Widget>[
+// TODO: Handle overflowing labels (103)
+            Text(
+              product.name,
+              style: theme.textTheme.button,
+              softWrap: false,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            const SizedBox(height: 4.0),
+            Text(
+              formatter.format(product.price),
+              style: theme.textTheme.caption,
+            ),
+            // End new code
+          ],
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
                   child: Column(
                     // TODO: Align labels to the bottom and center (103)
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     // TODO: Change innermost Column (103)
                     children: <Widget>[
